1ffc69853767c8e7a15ebf01fc1cc075e4e2bd59
haonc <haodev.01@gmail.com>
2024-09-29 15:41:36 +0700

    add google font

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,15 @@
 import { Button } from '@/components/ui/button';
+import { ListRoute } from '@/configs';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <Button>
-        <Link href="/signin"> Sigin</Link>
+        <Link href={ListRoute.SIGNIN}> Sigin</Link>
       </Button>
       <Button>
-        <Link href="/register">Register</Link>
+        <Link href={ListRoute.REGISTER}>Register</Link>
       </Button>
     </div>
   );
---
+++ configs/route.ts
@@ -0,0 +1,5 @@
+export const ListRoute = {
+  HOME: '/',
+  SIGNIN: '/signin',
+  REGISTER: '/register',
+};
