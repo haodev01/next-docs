9440946540950fb10133854d97eb8298ee008199
haonc <haodev.01@gmail.com>
2024-09-29 15:14:56 +0700

    Add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div>
+      <Button>Dashboard</Button>
       <h1>Hello Next.js!</h1>
     </div>
   );
