ca7d3a960e630cb33c2668cf90be61c887322105
haonc <haodev.01@gmail.com>
2024-09-29 15:22:25 +0700

    Add Layout and header

---
+++ components/blocks/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="pb-6">
+      <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+          Header
+        </h1>
+      </div>
+    </header>
+  );
+};
